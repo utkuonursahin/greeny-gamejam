7ff73f3a1e67dce10166ffc5f8a5492897f37b71
utkuonursahin <sahinutkuonur@gmail.com>
2022-09-06 13:56:50 +0300

    mobile-menu initial bug fix

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,7 +1,7 @@
 import {NavLink} from "react-router-dom";
 function MobileMenu({isClicked,setIsClicked}) {
   return (
-    <nav className={isClicked ? "mobile-menu mobile-menu--active" : "mobile-menu mobile-menu--disabled"}>
+    <nav className={`mobile-menu ${isClicked ? "mobile-menu--active" : ''}`}>
       <ul className="mobile-menu__list" onClick={() => setIsClicked(prev => !prev)} >
         <li className="mobile-menu__item">
           <NavLink className="mobile-menu__link" to="/" activeClassName="mobile-menu__link--active">Ana Sayfa</NavLink>
