9ae2933be793b370290133b9ab050d7bc3b0f3b8
utkuonursahin <sahinutkuonur@gmail.com>
2022-09-14 21:04:55 +0300

    responsive seperators

--- src/components/Organizers/Organizer/Organizer.jsx
+++ src/components/Organizers/Organizer/Organizer.jsx
@@ -1,29 +1,15 @@
-function Organizer({logo, itch, youtube, instagram, discord, linkedin, twitter, facebook}) {
+function Organizer(props) {
   return (
     <div className="organizer">
-      <img src={logo} alt="organizer-logo" />
+      <img src={props.logo} alt="organizer-logo" />
       <div className="organizer__links">
-        {itch && <a href={itch} target="_blank" rel="noreferrer">
-          <img src="assets/itchio.svg" alt="itch icon" />
-        </a>}
-        {youtube && <a href={youtube} target="_blank" rel="noopener noreferrer">
-          <img src="assets/youtube.svg" alt="youtube icon"/>
-        </a>}
-        {instagram && <a href={instagram} target="_blank" rel="noopener noreferrer">
-          <img src="assets/instagram.svg" alt="instagram icon"/>
-        </a>}
-        {discord && <a href={discord} target="_blank" rel="noopener noreferrer">
-          <img src="assets/discord.svg" alt="discord icon"/>
-        </a>}
-        {linkedin && <a href={linkedin} target="_blank" rel="noopener noreferrer">
-          <img src="assets/linkedin.svg" alt="linkedin icon"/>
-        </a>}
-        {twitter && <a href={twitter} target="_blank" rel="noopener noreferrer">
-          <img src="assets/twitter.svg" alt="twitter icon"/>
-        </a>}
-        {facebook && <a href={facebook} target="_blank" rel="noopener noreferrer">
-            <img src="assets/facebook.svg" alt="facebook icon"/>
-        </a>}
+        {props.itch && <a href={props.itch} target="_blank" rel="noreferrer"><img src="assets/itchio.svg" alt="itch icon"/></a>}
+        {props.youtube && <a href={props.youtube} target="_blank" rel="noopener noreferrer"><img src="assets/youtube.svg" alt="youtube icon"/></a>}
+        {props.instagram && <a href={props.instagram} target="_blank" rel="noopener noreferrer"><img src="assets/instagram.svg" alt="instagram icon"/></a>}
+        {props.discord && <a href={props.discord} target="_blank" rel="noopener noreferrer"><img src="assets/discord.svg" alt="discord icon"/></a>}
+        {props.linkedin && <a href={props.linkedin} target="_blank" rel="noopener noreferrer"><img src="assets/linkedin.svg" alt="linkedin icon"/></a>}
+        {props.twitter && <a href={props.twitter} target="_blank" rel="noopener noreferrer"><img src="assets/twitter.svg" alt="twitter icon"/></a>}
+        {props.facebook && <a href={props.facebook} target="_blank" rel="noopener noreferrer"><img src="assets/facebook.svg" alt="facebook icon"/></a>}
       </div>
     </div>
   );
